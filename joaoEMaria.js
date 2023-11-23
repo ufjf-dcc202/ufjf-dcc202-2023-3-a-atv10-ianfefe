af3be4f70d7285f4a78ace0b28a4e11756e089d0
ianfefe <felix.ian@estudante.ufjf.br>
2023-11-23 12:02:28 -0300

    cria joaoEMaria.js com as funcoes

---
+++ joaoEMaria.js
@@ -0,0 +1,27 @@
+const { findSourceMap } = require("module");
+
+let joao = 1;
+let maria = 2;
+
+function getMaria(){
+    return maria;
+}
+function getJoao(){
+    return joao;
+}
+
+function setMaria(valor){
+    maria = valor;
+}
+function setJoao(valor){
+    joao = valor;
+}
+
+function deJoaoParaMaria(){
+    maria += joao;
+    joao = 0;
+}
+function deMariaParaJoao(){
+    joao += maria;
+    maria = 0;
+}
